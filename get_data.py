9bdd2901208ece0d66e75e24d3b457bcf07a2a73
Guadalupe Fernando Escutia Rodriguez <167017684+Goshujinsama8822@users.noreply.github.com>
2024-04-14 17:14:48 -0600

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -60,7 +60,6 @@ for page in tPages:
         i = 0
 
         print(url + "/" + page['href'])
-        print(str(i))
         
         #Iniciamos el segundo bucle de nuestro script
         for npage in newPages:
@@ -68,6 +67,7 @@ for page in tPages:
             #Si el enlace de descarga corresponde a un csv ejecutamos el siguiente código
             if str(npage['href'][-4:]) == ".csv":
                 durl = url + "/" + npage['href']
+                durl = durl.replace(" ", "%20")
                 
                 #Descomentar la siguiente sección si se quiere descargar los datos en bruto, solo tener en consideración que se trata de varios GBs de información (En un principio tenía pensado transformar los datos en local, pero al pensarlo un poco al final no me hizo sentido)
                 
@@ -94,36 +94,52 @@ for page in tPages:
                 print(durl)
                 
                 if i == 0:
-                    mData = pd.DataFrame()
-                    mData = pd.read_csv(durl.replace(" ", "%20"), encoding = "ISO-8859-1")
-                    mData["Minimo Historico"] = ""
-                    mData["Máximo Historico"] = ""
-                    mData["Promedio Historico"] = ""
-                    i = i + 1
+                    try:
+                        mData = pd.DataFrame()
+                        mData = pd.read_csv(durl, encoding = "ISO-8859-1")
+                        mData["Minimo Historico"] = ""
+                        mData["Máximo Historico"] = ""
+                        mData["Promedio Historico"] = ""
+                        i = i + 1
+                        continue
+                    except pd.errors.ParserError as e:
+                        raise SystemExit(e)
                 else:
-                    data = pd.DataFrame()
-                    data = pd.read_csv(durl.replace(" ", "%20"), encoding="utf8", header = 6)
-                    if " Precipitación(mm)" in data.columns:
-                        data.rename(columns={" Precipitación(mm)" : "Precipitación(mm)"}, inplace=True)
-                    if "Gasto(m³/s)" in data.columns:
-                        data.rename(columns={"Gasto(m³/s)" : "Precipitación(mm)"}, inplace=True)
-                    if " Gasto(m³/s)" in data.columns:
-                        data.rename(columns={" Gasto(m³/s)" : "Precipitación(mm)"}, inplace=True)
+                    try:
+                        data = pd.DataFrame()
+                        data = pd.read_csv(durl, encoding="utf8", header = 6)
+                        if " Precipitación(mm)" in data.columns:
+                            data.rename(columns={" Precipitación(mm)" : "Precipitación(mm)"}, inplace=True)
+                        if "Gasto(m³/s)" in data.columns:
+                            data.rename(columns={"Gasto(m³/s)" : "Precipitación(mm)"}, inplace=True)
+                        if " Gasto(m³/s)" in data.columns:
+                            data.rename(columns={" Gasto(m³/s)" : "Precipitación(mm)"}, inplace=True)
+                        i = i + 1
+                    except pd.errors.ParserError as e:
+                        raise SystemExit(e)
                 
                 j = 0
-                k = 0
-
-                #print(str(len(mData.iloc[:,1])))
-
+                
                 for j in range(0, len(mData.iloc[:,1])):
                     if mData.iloc[j,1] == str(npage['href'][17:]).replace(".csv", ""):
-                        print(str(npage['href'][17:]).replace(".csv", ""))
+                        print("Encontrado" + str(npage['href'][17:]).replace(".csv", "") + ", procesando...")
                         data['Precipitación(mm)'] = data['Precipitación(mm)'].replace("-", np.nan)
                         data['Precipitación(mm)'] = pd.to_numeric(data['Precipitación(mm)'], errors="coerce")
                         mData.iloc[j,12] = data['Precipitación(mm)'].min()
                         mData.iloc[j,13] = data['Precipitación(mm)'].max()
                         mData.iloc[j,14] = data['Precipitación(mm)'].mean()
-                    else:
-                        mData.drop([j])
+                        continue
+                
+            # if i == 5:
+            #     break
+            
+        k = 0
+        dlist = []
+            
+        for k in range(0, len(mData["Minimo Historico"])):
+            if mData.loc[mData.index[k], "Minimo Historico"] == "":
+                dlist.append(k)
+                
+        mData.drop(dlist)
                         
-        mData.to_csv(page['href'][:-5] + "concentrado-" + page['href'][:-5] + ".csv")
+        mData.to_csv(page['href'][:-5] + "-concentrado-" + page['href'][:-5] + ".csv")
